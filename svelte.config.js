56c23a877a3c79dd4e83b1465bdbd15818c9f7cb
Maxime Quandalle <maxime@quandalle.com>
2021-12-30 00:29:27 +0100

    Add a note about json.stringify config option

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,22 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from "@sveltejs/adapter-auto";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter(),
+  kit: {
+    adapter: adapter(),
 
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
-	}
+    // hydrate the <div id="svelte"> element in src/app.html
+    target: "#svelte",
+
+    vite: {
+      json: {
+        // Note: The probleme is present with both
+        // `stringify: true` and `stringify: false`
+        // https://vitejs.dev/config/#json-stringify
+        stringify: true,
+      },
+    },
+  },
 };
 
 export default config;
